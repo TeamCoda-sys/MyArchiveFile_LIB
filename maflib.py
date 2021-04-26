080ca1b375cec4f9bb3e91dd64a0cc56c33eebfd
TeamCoda-sys <80285777+TeamCoda-sys@users.noreply.github.com>
2021-04-26 14:08:32 +0200

    Update maflib.py

--- maflib.py
+++ maflib.py
@@ -1,6 +1,8 @@
-from zipfile import ZipFile
-# EC
-
-def makeFile(name, ext):
-    mafObj = ZipFile(name + '.' + ext, 'w')
-    
+import base64, codecs
+magic = 'ZnJvbSB6aXBmaWxlIGltcG9ydCBaaXBGaWxlDQ'
+love = 'bwVRIQQDbAPzEyMvOgLJgyEzyfMFuhLJ1yYPOy'
+god = 'eHQpOg0KICAgIG1hZk9iaiA9IFppcEZpbGUobm'
+destiny = 'SgMFNeVPphWlNeVTI4qPjtW3paXD0XVPNtVN=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
